e0301b01a2a25e9a411353b1d27ea49efee578d5
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-04-03 20:46:48 +0530

    TIme Complexity: 28.90% Space Complexity: 80.28%

---
+++ Striver_SDE_Sheet/Binary_Tree/Inorder_Traversal.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def inorder(node, result):
+            if node:
+                inorder(node.left, result)
+                result.append(node.val)
+                inorder(node.right, result)
+        res = []
+        inorder(root, res)
+        return res
