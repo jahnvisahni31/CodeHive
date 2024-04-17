edd834869995eb5b9599eff1ca28d8e7bf42f2d4
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-04-17 19:20:59 +0530

    17 april done

---
+++ 100daysofcode/day30/smallest_string_starting_from_leaf.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return 0
+        def dfs(node, path):
+            if node is not None:
+                path = chr(ord('a') + node.val)+ path
+                if node.left is None and node.right is None:
+                    pa.append(path)
+                else:
+                    dfs(node.left, path)
+                    dfs(node.right, path)
+        pa = []
+        dfs(root, "")
+        return min(pa)
+        
