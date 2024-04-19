bd7eeced35f427f9d208e13db97ddce852b16db6
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-04-19 22:05:54 +0530

    Create 0002_Missing_number_in_array.py

---
+++ GFG_sde_sheet/0002_Missing_number_in_array.py
@@ -0,0 +1,9 @@
+
+
+class Solution:
+    def missingNumber(self,array,n):
+        # code here
+        expected_sum = (n * (n + 1)) // 2
+        actual_sum = sum(array)
+        missing_element = expected_sum - actual_sum
+        return missing_element
