585e7bddc19d92ac7ba3035d3bf1e357d8923322
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-04-28 04:53:48 +0530

    space complexity: 99.28% time complexity: 68.35%

---
+++ 100daysofcode/Minimum Number of Moves to Seat Everyone/Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        t = sum(abs(seat-student) for seat, student in zip(seats, students))
+        return t
